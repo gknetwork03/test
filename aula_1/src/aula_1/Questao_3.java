205fa134c86c3a040c38818a7b52a63e1f6d3b55
Gk Network <gknetwork03@outlook.com.br>
2023-07-04 16:44:28 -0400

    Atualizado 1

---
+++ aula_1/src/aula_1/Questao_3.java
@@ -0,0 +1,17 @@
+package aula_1;
+
+public class Questao_3 {
+
+	
+		public static void main(String[] args)
+		{
+		   int n = 3 ;
+		   
+		   for(int aux = 1;aux<=10;aux++)
+		  {
+		     System.out.println(n+" x "+aux+" = "+n*aux);
+		  }
+		  System.out.println("\nFim");
+		}
+	}
+
