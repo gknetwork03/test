395497ce7bb73ce519a57d1a00a0d03fd03b12ae
Gk Network <gknetwork03@outlook.com.br>
2023-07-04 17:20:46 -0400

    Atualização 3

---
+++ aula_1/src/aula_1/Questao_4.java
@@ -0,0 +1,34 @@
+package aula_1;
+import java.util.Scanner;
+
+public class Questao_4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner lerNum = new Scanner(System.in);
+		 int marcaA = 0;
+		 int marcaB = 0;
+		 int marcaC = 0;
+		System.out.println(" Digite a quantidade de Chuteira Marca A: ");
+		    marcaA = lerNum.nextInt();
+		    
+		    System.out.println(" Digite a quantidade de Chuteira Marca B: ");
+		    marcaB = lerNum.nextInt();
+		    
+		    System.out.println(" Digite a quantidade de Chuteira Marca C: ");
+		    marcaC = lerNum.nextInt();
+		    
+		    System.out.println("Existem: "+  marcaA );
+		    System.out.println("Existem: "+  marcaB);
+		    System.out.println("Existem: "+  marcaC);
+		    
+		   // System.out.println("Existem"+  marcaA "chuteira da Marca A," + marcaB "da Marca B" + marcaC "da marca C" );
+		    
+		    			System.out.println("Quantidade de chuteira em Estoque");
+		    			System.out.println("marca A " + marcaA);
+		    			System.out.println("marca B " + marcaB);
+		    			System.out.println("marca C " + marcaC);
+}
+	}
+		
